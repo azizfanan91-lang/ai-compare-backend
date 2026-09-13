ce47730597772f2168b884deadb6ad5a062fc9b2
azizfanan91-lang <azizfanan91@gmail.com>
2026-09-13 12:54:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const authRoutes = require('./authRoutes');
 const chatRoutes = require('./chatRoutes');
 const adminRoutes = require('./adminRoutes');
+const paymentRoutes = require('./paymentRoutes');
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', chatRoutes);
 app.use('/api/admin', adminRoutes);
+app.use('/api/payment', paymentRoutes);
 
 app.get('/', (req, res) => {
   res.json({ status: 'ok', message: 'AI Compare Backend خدام' });
